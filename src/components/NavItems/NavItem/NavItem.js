d54c58ab4282c81fa939992cf08aee43f9283ba0
Mohamed_Sherif <helfer996@gmail.com>
2023-04-23 23:36:42 +0200

    fixed a tag

--- src/components/NavItems/NavItem/NavItem.js
+++ src/components/NavItems/NavItem/NavItem.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 const navItem = (props) => {
   return (
     <div className={styles.item}>
-      <a to={props.to || "/"}><p>{props.itemName}</p></a>
+      <a href={props.to || "/"}><p>{props.itemName}</p></a>
     </div>
   )
 }; export default navItem;
